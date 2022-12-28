1a45b46c8eaf4d9ff75d75eb2dd04fcbbc5b4530
Евгений Корецкий <mcgeenerman@gmail.com>
2022-12-28 04:42:15 +0200

    initial commit with impl

---
+++ TestHttpService/HttpServiceSample.cs
@@ -0,0 +1,95 @@
+﻿namespace TestHttpService
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Net;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class HttpServiceSample
+    {
+        readonly int port = 80;
+
+        readonly CancellationTokenSource cancellationSource = new();
+
+        readonly HttpListener listener = new();
+
+        public Func<HttpListenerRequest, HttpListenerResponse, bool>? OnRequest { get; set; }
+
+        public HttpServiceSample()
+        {
+            listener.Prefixes.Add($"http://+:{port}/TestService/");
+        }
+
+        Task? runTask;
+
+        public bool StartAsync()
+        {
+            if (runTask?.IsCompleted == false)
+            {
+                return false;
+            }
+            runTask = Task.Run(Start);
+            return true;
+        }
+
+        public void Stop()
+        {
+            cancellationSource.Cancel();
+            runTask?.Wait();
+
+        }
+
+        void Start()
+        {
+            listener.Start();
+
+            Console.WriteLine($"Listening on port {port}...");
+
+            try
+            {
+                while (true)
+                {
+                    var task = listener.GetContextAsync();
+
+                    try
+                    {
+                        task.Wait(cancellationSource.Token);
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        Console.WriteLine("Listening Operation Canceled");
+                        break;
+                    }
+
+                    if (task.IsCompleted)
+                    {
+                        HttpListenerContext ctx = task.Result;
+
+
+                        using (HttpListenerResponse response = ctx.Response)
+                        {
+                            if ((OnRequest?.Invoke(ctx.Request, response) ?? false) == false)
+                            {
+                                response.StatusCode = (int)HttpStatusCode.NotFound;
+                                response.StatusDescription = "Not found";
+                            }
+                            else
+                            {
+                                response.StatusCode = (int)HttpStatusCode.OK;
+                                response.StatusDescription = "Status OK";
+                            }
+                        }
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ TestHttpService/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using TestHttpService;
+
+var httpService = new HttpServiceSample();
+
+var t = httpService.StartAsync();
+
+int counter = 0;
+
+httpService.OnRequest += (HttpListenerRequest request, HttpListenerResponse response) =>
+{
+    Console.WriteLine($"Process request #{counter} ...");
+
+    var validate = request.QueryString.Get("validate");
+
+    if (string.IsNullOrEmpty(validate) == false)
+    {
+        ++counter;
+
+        if (counter % 4 == 0) // every time after 4-times attempts (if refresh 1 seconds, it mean every 4 seconds return OK result)
+        {
+            Console.WriteLine(".... Simulate Image Data was changed!!!");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine(".... Simulate Image Data not changed yet....");
+            return false;
+        }
+    }
+    else
+    {
+        var filename = request.Url?.Segments.Last();
+
+        if (!File.Exists(filename))
+        {
+            filename = "index.html";
+
+            response.AddHeader("Content-Type", "text/html; charset=utf-8");
+            response.AddHeader("Cache-Control", "max-age=1500, must-revalidate");
+        }
+        else
+        {
+            Console.WriteLine(".... Simulate Reloading Image Data ....");
+            Console.WriteLine($"Resource: {filename}");
+            Console.WriteLine(request.Headers);
+
+            response.AddHeader("Content-Type", "image/jpg");
+            response.AddHeader("Date", DateTime.Now.ToString("r"));
+            response.AddHeader("Last-Modified", File.GetLastWriteTime(filename).ToString("r"));
+            response.AddHeader("Cache-Control", "no-cache");
+        }
+
+        using FileStream file = new FileStream(filename, FileMode.Open, FileAccess.Read);
+        var buffer = new byte[file.Length];
+        file.Read(buffer, 0, buffer.Length);
+
+        using Stream output = response.OutputStream;
+        output.Write(buffer, 0, buffer.Length);
+    }
+
+    return true;
+};
+
+
+Console.ReadLine();
+
+httpService.Stop();
+
